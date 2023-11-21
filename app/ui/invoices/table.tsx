d4d6321c1a394983b8f9058b5e82fd4967c9274c
Felipe Gomes <contato@felipe-gomes.com>
2023-11-21 09:27:28 -0300

    Alt on invoice table image

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -32,7 +32,7 @@ export default async function InvoicesTable({
                         width={28}
                         height={28}
                         alt={`${invoice.name}'s profile picture`}
-                      />
+                      /> 
                       <p>{invoice.name}</p>
                     </div>
                     <p className="text-sm text-gray-500">{invoice.email}</p>
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt={`${invoice.name}'s profile picture`}
                       />
                       <p>{invoice.name}</p>
                     </div>
